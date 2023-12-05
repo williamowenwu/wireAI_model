8a5b39a3059db6375959d14b0f9cea25155b9b3c
Bavi Tyagi <bt335@c211-1.cs.rutgers.edu>
2023-12-04 21:34:40 -0500

    progress

--- wire.py
+++ wire.py
@@ -8,25 +8,32 @@ class Wire_Grid():
         self.D = 20 # 20 x 20  Dimension of ship
         self.grid = []
 
-        # colors possible in the ship, represented by indices, 0 - 3
-        self.colors = ["RED", "BLUE", "YELLOW", "GREEN"]
-
-
-
+        # colors possible in the grid
+        self.colors = ["R", "B", "Y", "G", "W"]
 
     def generate_Grid(self) -> None:
-        self.grid = [["-1" for _ in range(self.D)] for _ in range(self.D)]
+        self.grid = [["W" for _ in range(self.D)] for _ in range(self.D)]
+        
+        row_col = random.randint(0,19)
+        
 
 
-        pass
+    # This the random grid layout
+    # PREDICTION: Not Dangerous / Dangerous
+    # wrire to cut
+
+    def print_grid(self) -> None:
+        for x in range(grid.D):
+            print(" ", end="")
+            for y in range(grid.D):
+                if grid.grid[x][y] == "W": 
+                    print(Back.WHITE + " ", end = " ")     
+            print(Back.RESET) 
 
 
 if __name__ == "__main__":
 
     grid = Wire_Grid()
     grid.generate_Grid()
-    for x in range(20):
-        print(" ", end="")
-        for y in range(20):
-            print(grid.grid[x][y] + "  ", end="")
+    grid.print_grid()
     
