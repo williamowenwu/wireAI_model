18dcb098271226271d7735f3af3434e7596a69e3
William Wu <willwu1217@gmail.com>
2023-12-04 22:33:40 -0500

    set up complete

--- wire.py
+++ wire.py
@@ -8,15 +8,39 @@ class Wire_Grid():
         self.D = 20 # 20 x 20  Dimension of ship
         self.grid = []
 
-        # colors possible in the grid
-        self.colors = ["R", "B", "Y", "G", "W"]
+        # colors possible in the grid the colors being used will be indices 0 - 3
+        self.colors = ["R", "B", "Y", "G"]
+        self.used = []
+        self.wire_order = []
 
     def generate_Grid(self) -> None:
         self.grid = [["W" for _ in range(self.D)] for _ in range(self.D)]
-        
-        row_col = random.randint(0,19)
-        
+        # Choose either a row or a column randomly
+        row_col = random.randint(0, 1)
 
+        # Iterate through all colors
+        for _ in range(len(self.colors)):  # Subtract 1 to exclude the 'W' color
+            if row_col == 1:
+                # row
+                row = random.randint(0, self.D - 1)
+                color = self.choose_unused_color()
+                self.used.append(color)
+                self.grid[row] = [color] * self.D
+                row_col = 0
+            else:
+                # col
+                col = random.randint(0, self.D - 1)
+                color = self.choose_unused_color()
+                self.used.append(color)
+                for x in range(self.D):
+                    self.grid[x][col] = color
+                row_col = 1
+                    
+    def choose_unused_color(self):
+        while True:
+            color = random.choice(self.colors)
+            if color not in self.used:
+                return color
 
     # This the random grid layout
     # PREDICTION: Not Dangerous / Dangerous
@@ -27,7 +51,15 @@ class Wire_Grid():
             print(" ", end="")
             for y in range(grid.D):
                 if grid.grid[x][y] == "W": 
-                    print(Back.WHITE + " ", end = " ")     
+                    print(Back.WHITE + " ", end = " ")
+                elif grid.grid[x][y] == "G": 
+                    print(Back.GREEN + " ", end = " ")
+                elif grid.grid[x][y] == "Y": 
+                    print(Back.YELLOW + " ", end = " ") 
+                elif grid.grid[x][y] == "B": 
+                    print(Back.CYAN + " ", end = " ") 
+                elif grid.grid[x][y] == "R": 
+                    print(Back.RED + " ", end = " ")    
             print(Back.RESET) 
 
 
